7a1f4ea8d4cc98b626bc825b6699438c1f0f15d3
EyalKoubi <109479738+EyalKoubi@users.noreply.github.com>
2025-03-16 17:48:47 +0200

    Update EditProblemModal.js

--- frontend/src/components/EditProblemModal.js
+++ frontend/src/components/EditProblemModal.js
@@ -11,7 +11,7 @@ const EditProblemModal = ({ problem, onClose, onSave, setProblems }) => {
 
     try {
       const res = await fetch(
-        `http://localhost:3002/admin/problems/${problem._id}`,
+        `https://leetcode-project-backend.onrender.com/admin/problems/${problem._id}`,
         {
           method: "PUT",
           headers: {
